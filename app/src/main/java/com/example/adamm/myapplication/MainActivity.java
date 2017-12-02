5b1646186914ae9bfbaacb96fc728cb5b886d66f
Adam Mołczan <adam.molczan@outlook.com>
2017-12-02 20:00:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/adamm/myapplication/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.adamm.myapplication;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Menu appMenu = null;
+    TextView tv = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showMessageButton();
+
+        tv = (TextView)this.findViewById(R.id.textViewId);
+        tv.setText("Menu kontekstowe");
+        registerForContextMenu(tv);
+    }
+
+    public void showMessageButton(){
+        Button btn1 = (Button) findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "Nacisnąłeś przycisk",
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu); //ładowanie menu z pliku xml
+        appMenu = menu;
+        AddRegularMenuItem(appMenu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_start) {
+            return true;
+        }else if (id == R.id.menu_message) {
+            tv.setText("Wiadomość z aplikacji");
+            tv.setTextColor(Color.RED);
+            tv.setTextSize(50);
+        }else if (id == R.id.menu_exit) {
+            finish();
+            System.exit(0);
+        } else if (id == 100){
+            Toast.makeText(
+                    MainActivity.this,
+                    "Nacisnąłeś przycisk dodany programowo",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void AddRegularMenuItem(Menu menu) {
+        int base=Menu.FIRST;
+        menu.add(base,100,100,"Pozycja dodana programowo");
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo
+            menuInfo) {
+        menu.setHeaderTitle("Menu kontekstowe");
+        menu.add(200, 200, 200, "Element 1");
+        menu.add(300, 300, 300, "Element 2");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == 200) {
+            tv.append("\n Kontekstowe 1");
+        }else if (id == 300) {
+            tv.append("\n Kontekstowe 2");
+        }
+        return super.onContextItemSelected(item);
+    }
+}
